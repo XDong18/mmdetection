c073ef9072cbc6af69c073e79b87ebdbda2f7c6f
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-09-27 07:26:04 +0800

    add gpu numbers

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-python tools/train.py configs/ssd/ssd512_bdd100k.py 4 --work-dir out/ssd
+python tools/train.py configs/ssd/ssd512_bdd100k.py --gpus 4 --work-dir out/ssd
