abfe0475b65fe5d01dedc8676595977911a0f8bb
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-09-27 18:54:52 +0800

    ssd300 1x

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-./tools/dist_train.sh configs/ssd/ssd512_bdd100k.py 4 --work-dir out/ssd_2x
+./tools/dist_train.sh configs/ssd/ssd300_bdd100k.py 4 --work-dir out/ssd300_1x
