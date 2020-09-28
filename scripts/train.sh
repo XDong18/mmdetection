64c5932528f54b97f9cddb96ad17870aa7cc6407
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-09-29 01:24:59 +0800

    yolov3 608

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-./tools/dist_train.sh configs/ssd/ssd300_bdd100k.py 4 --work-dir out/ssd300_1x
+export CUDA_VISIBLE_DEVICES=0,1,7
+./tools/dist_train.sh cconfigs/yolo/yolov3_d53_mstrain-608_273e_bdd100k.py 3 --work-dir out/yolov3_608_273e
