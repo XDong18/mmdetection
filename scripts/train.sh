32303598a5ced36b05672d3d6ae35770c078db58
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-02 23:45:20 +0800

    cascade 100k train sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-./tools/dist_train.sh configs/yolo/yolov3_d53_mstrain-608_273e_bdd100k.py 4 --work-dir out/yolov3_608_273e --resume-from out/yolov3_608_273e/epoch_30.pth
+./tools/dist_train.sh configs/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_1x_bdd100k.py 4 --work-dir out/cascade_x101_64x4d_fpn_1x
