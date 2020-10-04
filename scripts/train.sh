1801ec847cd5f98dcb021107da1e4af00b6ac4a5
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-04 22:22:01 +0800

    ssd512 10x

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-./tools/dist_train.sh configs/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_1x_bdd100k.py 4 --work-dir out/cascade_x101_64x4d_fpn_1x
+./tools/dist_train.sh configs/ssd/ssd512_bdd100k_10x.py 4 --work-dir out/ssd_512_10x
