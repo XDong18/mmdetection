bfa7eb13de5f1b7ede97beef34476132aff328d8
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-09 15:08:51 +0800

    cascade_rcnn_r101_fpn_1x

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-./tools/dist_train.sh configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_bdd100k.py 4 --work-dir out/cascade_rcnn_r50_fpn_1x
+./tools/dist_train.sh configs/cascade_rcnn/cascade_rcnn_r101_fpn_1x_bdd100k.py 4 --work-dir out/cascade_rcnn_r101_fpn_1x
