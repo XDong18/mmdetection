851387d620a1c955144fd75e55c8bacb89cf7db0
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-10 13:57:45 +0800

    fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-./tools/dist_train.sh configs/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x_bdd100k.py 4 --work-dir out/cascade_rcnn_x101_32x4d_fpn_1x
+./tools/dist_train.sh configs/fcos/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x_bdd100k.py 4 --work-dir out/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x
