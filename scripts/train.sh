e09537fdb5f489e5b829440c6db6e686db0e2206
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-14 23:10:19 +0800

    faster_rcnn_r101_fpn_1x

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=1,2,3,5
 ./tools/dist_train.sh configs/faster_rcnn/faster_rcnn_r101_fpn_1x_bdd100k.py 4 --work-dir out/faster_rcnn_r101_fpn_1x
