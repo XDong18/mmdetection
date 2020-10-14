8d1651b4db8d9b5c8b85141ecfb1b116f1a3c9f3
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-14 23:07:38 +0800

    faster_rcnn_r101_fpn_1x

---
+++ configs/faster_rcnn/faster_rcnn_r101_fpn_1x_bdd100k.py
@@ -0,0 +1,2 @@
+_base_ = './faster_rcnn_r50_fpn_1x_bdd100k.py'
+model = dict(pretrained='torchvision://resnet101', backbone=dict(depth=101))
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-./tools/dist_train.sh configs/dcn/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x_bdd100k.py 4 --work-dir out/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x
+./tools/dist_train.sh configs/faster_rcnn/faster_rcnn_r101_fpn_1x_bdd100k.py 4 --work-dir out/faster_rcnn_r101_fpn_1x
