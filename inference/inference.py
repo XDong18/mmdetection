8d8b83c279c3c0c350bfc617ed019264a165a4f4
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-21 02:17:27 +0800

    inference

--- inference/inference.py
+++ inference/inference.py
@@ -1,7 +1,7 @@
 from mmdet.apis import init_detector, inference_detector
 import mmcv
 import os
-import tdqm
+import tqdm
 
 config_file = 'configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_bdd100k.py'
 checkpoint_file = 'out/cascade_rcnn_r50_fpn_1x/cascade_rcnn_r50_fpn_1x-b915cc22.pth'
@@ -17,7 +17,7 @@ out_dir = 'infer_img/val/cascade_rcnn_r50_fpn_1x' # TODO change out dir
 os.makedirs(out_dir)
 # img = 'test.jpg'  # or img = mmcv.imread(img), which will only load it once
 
-for img in tdqm(img_list):
+for img in tqdm(img_list):
     img_path = os.path.join(val_dir, img)
     result = inference_detector(model, img_path)
     out_path = os.path.join(out_dir, img)
