d2396ab2cd5838cbf97d6d35c10ac66db3f32f61
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-21 05:01:07 +0800

    html cascade_rcnn_r50_fpn_1x

--- visualization/auto_html.py
+++ visualization/auto_html.py
@@ -1,3 +1,15 @@
-out_fn = 'test.html'
+import os
+
+img_dir = 'infer_img/val/cascade_rcnn_r50_fpn_1x'
+img_list = os.listdir(img_dir)
+out_fn = 'visualization/cascade_rcnn_r50_fpn_1x.html'
 with open (out_fn, 'w') as f:
-    f.write('')
+    f.write('<html>\n')
+    f.write('<body>\n')
+    f.write('<h1>this is a line.</h1>\n')
+
+    for img in img_list:
+        new_line = "<img src='../" + os.path.join(img_dir, img) + "'>\n"
+        f.write(new_line)
+    f.write('</body>\n')
+    f.write('</html>\n')
