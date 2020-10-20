5bc9edef6374cddf014f6acdd2b2db1db7e57961
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-21 06:42:26 +0800

    yolo html

--- inference/inference.py
+++ inference/inference.py
@@ -3,9 +3,9 @@ import mmcv
 import os
 from tqdm import tqdm
 
-model_name = 'faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x'
-config_file = 'configs/dcn/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x_bdd100k.py'
-checkpoint_file = 'out/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x/latest.pth'
+model_name = 'yolov3_608_273e'
+config_file = 'configs/yolo/yolov3_d53_mstrain-608_273e_bdd100k.py'
+checkpoint_file = 'out/yolov3_608_273e/yolov3_608_273e-8c2596a1.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:7') # TODO
