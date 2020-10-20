5f220f526e8ebf426932cda43c67004eb3ef5abb
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-21 06:26:25 +0800

    faster_rcnn html

--- inference/inference.py
+++ inference/inference.py
@@ -3,9 +3,9 @@ import mmcv
 import os
 from tqdm import tqdm
 
-model_name = 'cascade_rcnn_r101_fpn_1x'
-config_file = 'configs/cascade_rcnn/cascade_rcnn_r101_fpn_1x_bdd100k.py'
-checkpoint_file = 'out/cascade_rcnn_r101_fpn_1x/cascade_rcnn_r101_fpn_1x-8f6ef242.pth'
+model_name = 'faster_rcnn_r101_fpn_1x'
+config_file = 'configs/faster_rcnn/faster_rcnn_r101_fpn_1x_bdd100k.py'
+checkpoint_file = 'out/faster_rcnn_r101_fpn_1x/latest.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:7') # TODO
