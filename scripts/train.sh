c44e44330fccf7a1bce4d8fe9250e8809f82013e
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-20 17:20:47 +0800

    cascade_rcnn_r50_fpn_1x_1280

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-PORT=29501 ./tools/dist_train.sh configs/dcn/faster_rcnn_x101_32x4d_fpn_dconv_c3-c5_1x_bdd100k.py 4 --work-dir out/faster_rcnn_x101_32x4d_fpn_dconv_c3-c5_1x
+PORT=29501 ./tools/dist_train.sh configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_1280_bdd100k.py 4 --work-dir out/cascade_rcnn_r50_fpn_1x_1280
