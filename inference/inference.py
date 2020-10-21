47860c90aaa4b417ea6239fc13d7f57cce0bd9dd
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-21 13:19:46 +0800

    fcos_center-normbbox-centeronreg-giou_r101_caffe_fpn_gn-head_dcn_4x4_1x  html

--- inference/inference.py
+++ inference/inference.py
@@ -4,7 +4,7 @@ import os
 from tqdm import tqdm
 
 model_name = 'cascade_rcnn_x101_32x4d_fpn_1x'
-config_file = 'configs/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x'
+config_file = 'configs/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x_bdd100k.py'
 checkpoint_file = 'out/cascade_rcnn_x101_32x4d_fpn_1x/cascade_rcnn_x101_32x4d_fpn_1x-46de620b.pth'
 
 # build the model from a config file and a checkpoint file
