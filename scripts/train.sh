97b12d186a8d34829cb43bf09ee947619e083d7d
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-22 23:36:50 +0800

    faster_rcnn_r50_fpn_1x

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=4,5,6,7
-PORT=29501 configs/faster_rcnn/faster_rcnn_r50_fpn_1x_bdd100k.py 4 --work-dir out/faster_rcnn_r50_fpn_1x
+PORT=29501 ./tools/dist_train.sh configs/faster_rcnn/faster_rcnn_r50_fpn_1x_bdd100k.py 4 --work-dir out/faster_rcnn_r50_fpn_1x
