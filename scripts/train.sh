a0606dad5f7b1ab4c6538a932b757f82357123bc
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-25 01:42:53 +0800

    faster_rcnn_x101_32x8d_fpn_1x

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=1,2,8,9
-PORT=29501 ./tools/dist_train.sh configs/retinanet/retinanet_r101_fpn_1x_bdd100k.py 4 --work-dir out/retinanet_r101_fpn_1x
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+PORT=29501 ./tools/dist_train.sh configs/faster_rcnn/faster_rcnn_x101_32x8d_fpn_1x_bdd100k.py 4 --work-dir out/faster_rcnn_x101_32x8d_fpn_1x
