d350e9711541b1f411346827ad99dc7d8f300ade
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-28 02:02:54 +0800

    eval ssd 10x

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,9
 
-./tools/dist_test.sh configs/ssd/ssd512_bdd100k_10x.py out/ssd_512_10x/ssd_512_10x-91845fef.pth 2 --eval bbox --eval-options "classwise=True"
+PORT=29502 ./tools/dist_test.sh configs/ssd/ssd512_bdd100k_10x.py out/ssd_512_10x/ssd_512_10x-91845fef.pth 2 --eval bbox --eval-options "classwise=True"
