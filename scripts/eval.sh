a12be03810456701ccfe6d416399f54a33c898c4
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-28 10:09:41 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,9
 
-PORT=29502 ./tools/dist_test.sh configs/ssd/ssd512_bdd100k_10x.py out/ssd_512_10x/ssd_512_10x-91845fef.pth 2 --eval bbox --eval-options "classwise=True"
+PORT=29502 ./tools/dist_test.sh configs/cascade_rcnn/cascade_rcnn_x101_64x4d_fpn_1x_bdd100k.py out/cascade_x101_64x4d_fpn_1x/cascade_x101_64x4d_fpn_1x-d0f391c7.pth 2 --eval bbox --eval-options "classwise=True"
