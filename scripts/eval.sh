24a41d1f1eab27a2f30e9f788226e89cecc19ffd
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-28 13:12:14 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,9
 
-PORT=29502 ./tools/dist_test.sh configs/retinanet/retinanet_r50_fpn_1x_bdd100k.py out/c_out/retinanet_r50_fpn_1x/latest.pth 2 --eval bbox --eval-options "classwise=True"
+PORT=29502 ./tools/dist_test.sh configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_bdd100k.py a_out/cascade_rcnn_r50_fpn_1x/lcascade_rcnn_r50_fpn_1x-b915cc22.pth 2 --eval bbox --eval-options "classwise=True"
