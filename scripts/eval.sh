aa746349cb3334dcc73ab1076793c812bcbc4c83
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-28 11:00:32 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,9
 
-PORT=29502 ./tools/dist_test.sh configs/dcn/faster_rcnn_r101_fpn_dconv_c3-c5_1x_bdd100k.py out/faster_rcnn_r101_fpn_dconv_c3-c5_1x/latest.pth 2 --eval bbox --eval-options "classwise=True"
+PORT=29502 ./tools/dist_test.sh configs/fcos/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x_bdd100k.py out/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x/fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_4x4_1x-99d59f90.pth 2 --eval bbox --eval-options "classwise=True"
