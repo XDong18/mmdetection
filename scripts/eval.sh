ec5ceb07ddc51d521c0baaac63df088a40761a2b
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-10-28 12:50:56 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=4,9
 
-PORT=29502 ./tools/dist_test.sh configs/yolo/yolov3_d53_mstrain-608_273e_bdd100k.py a_out/yolov3_608_273e/yolov3_608_273e-8c2596a1.pth 2 --eval bbox --eval-options "classwise=True"
+PORT=29502 ./tools/dist_test.sh configs/retinanet/retinanet_r50_fpn_1x_bdd100k.py out/c_out/retinanet_r50_fpn_1x/latest.pth 2 --eval bbox --eval-options "classwise=True"
