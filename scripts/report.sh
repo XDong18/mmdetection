3f604a69591ef428fbf14f144b56a32187d09674
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 02:06:40 +0800

    cc

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,4 +1,5 @@
 export CUDA_VISIBLE_DEVICES=4,9
+export MKL_SERVICE_FORCE_INTEL=1
 python report_info.py a_out/faster_rcnn_r101_fpn_1x/latest.pth --out_name mmdetection-faster_rcnn_r101_fpn-1x --config configs/faster_rcnn/faster_rcnn_r101_fpn_1x_bdd100k.py
 python report_info.py out/faster_rcnn_x101_32x4d_fpn_1x/latest.pth --out_name mmdetection-faster_rcnn_x101_32x4d_fpn-1x --config configs/faster_rcnn/configs/faster_rcnn/faster_rcnn_x101_32x4d_fpn_1x_bdd100k.py
 python report_info.py c_out/retinanet_r50_fpn_1x/latest.pth --out_name mmdetection-retinanet_r50_fpn-1x --config configs/retinanet/retinanet_r50_fpn_1x_bdd100k.py
