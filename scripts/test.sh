b109b6aa320e409dac130ac1142521cb81167f9f
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-02 22:53:36 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=4,9
-./tools/dist_test.sh configs/faster_rcnn/faster_rcnn_r50_fpn_1x_bdd100k.py \
+PORT=29503 ./tools/dist_test.sh configs/faster_rcnn/faster_rcnn_r50_fpn_1x_bdd100k.py \
     out/faster_rcnn_r50_fpn_1x/latest.pth \
     2 --format-only --eval-options "jsonfile_prefix=./faster_rcnn_r50_fpn_1x_results"
