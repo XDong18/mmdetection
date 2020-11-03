94471f30e386c62daa70ff5f9af891111a1e87d7
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-03 17:05:24 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-PORT=29501 ./tools/dist_train.sh configs/pafpn/faster_rcnn_r101_pafpn_1x_bdd100k.py 4 --work-dir out/faster_rcnn_r101_pafpn_1x
+PORT=29503 ./tools/dist_train.sh configs/pafpn/faster_rcnn_r101_pafpn_1x_bdd100k.py 4 --work-dir out/faster_rcnn_r101_pafpn_1x
