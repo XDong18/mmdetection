7bdf37ed19bf1063663cbb439fd6835926d3efbf
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 04:40:02 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/yolo/yolov3_d53_mstrain-608_273e_bdd100k.py \
-    out/yolov3_608_273e/yolov3_608_273e-8c2596a1.pth \
+PORT=29503 ./tools/dist_test.sh configs/faster_rcnn/faster_rcnn_r101_fpn_1x_bdd100k.py \
+    out/faster_rcnn_r101_fpn_1x/latest.pth \
     4 --eval bbox --val_dataset
