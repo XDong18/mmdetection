ef340fc3a55d179eb6e07c7ce2cedc8bf5ea1faf
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 03:51:03 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/cascade_rcnn/cascade_rcnn_r101_fpn_1x_bdd100k.py \
-    out/cascade_rcnn_r101_fpn_1x/cascade_rcnn_r101_fpn_1x-8f6ef242.pth \
+PORT=29503 ./tools/dist_test.sh configs/cascade_rcnn/cascade_rcnn_x101_32x4d_fpn_1x_bdd100k.py \
+    out/cascade_rcnn_x101_32x4d_fpn_1x/cascade_rcnn_x101_32x4d_fpn_1x-46de620b.pth \
     4 --eval bbox
