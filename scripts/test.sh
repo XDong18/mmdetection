a6e759ce0f65fd5be9b78152bafd62ee571a52ae
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 05:35:39 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/fcos/fcos_center-normbbox-centeronreg-giou_r101_caffe_fpn_gn-head_dcn_4x4_1x_bdd100k.py \
-    b_out/fcos_center-normbbox-centeronreg-giou_r101_caffe_fpn_gn-head_dcn_4x4_1x/fcos_center-normbbox-centeronreg-giou_r101_caffe_fpn_gn-head_dcn_4x4_1x-9167d889.pth \
+PORT=29503 ./tools/dist_test.sh configs/faster_rcnn/faster_rcnn_x101_32x4d_fpn_1x_bdd100k.py \
+    b_out/faster_rcnn_x101_32x4d_fpn_1x/latest.pth \
     4 --eval bbox --val_dataset
