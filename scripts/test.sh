9b060e71f634264b26db293848968dd0832e4679
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 04:53:54 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/faster_rcnn/faster_rcnn_r101_fpn_1x_bdd100k.py \
-    out/faster_rcnn_r101_fpn_1x/latest.pth \
+PORT=29503 ./tools/dist_test.sh configs/dcn/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x_bdd100k.py \
+    out/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x/latest.pth \
     4 --eval bbox --val_dataset
