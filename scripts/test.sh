9f439c70fc2a26b9215ce21cda30891926563b47
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-11 05:02:53 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=6,7,8,9
-PORT=29503 ./tools/dist_test.sh configs/dcn/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x_bdd100k.py \
-    out/faster_rcnn_r50_fpn_mdconv_c3-c5_group4_1x/latest.pth \
+PORT=29503 ./tools/dist_test.sh configs/ssd/ssd512_bdd100k_10x.py \
+    b_out/ssd_512_10x/ssd_512_10x-91845fef.pth \
     4 --eval bbox --val_dataset
