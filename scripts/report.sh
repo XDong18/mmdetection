28c17c3ff35593aec98fa3639a3b601e58514d79
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-18 10:26:51 +0800

    cc

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=4,9
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export MKL_SERVICE_FORCE_INTEL=1
 python report_info.py b_out/cornernet_hourglass104_mstest_4x6_210e/latest.pth --out_name mmdetection-cornetnet_hourglass104-210e --config configs/cornernet/cornernet_hourglass104_mstest_4x6_210e_bdd100k.py
