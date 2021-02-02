0bc5627fcd983e8848994948373f81e84ca8d082
XDong18 <liuxd1215@gmail.com>
2021-02-03 01:23:24 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,8,9
 PORT=29503 ./tools/dist_train.sh \
 configs/reppoints/reppoints_moment_r101_fpn_dconv_c3-c5_gn-neck+head_1x_bdd100k.py \
-8 --work-dir out/reppoints_r101_bs16_bdd100k
+8 --work-dir out/reppoints_r101_bs16_bdd100k  --resume
