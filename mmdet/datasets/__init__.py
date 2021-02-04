42f77434c6e7d3cf19316d46195bed242d197d73
XDong18 <liuxd1215@gmail.com>
2021-02-05 03:19:27 +0800

    cc

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -8,7 +8,8 @@ from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
 from .deepfashion import DeepFashionDataset
 from .lvis import LVISDataset, LVISV1Dataset, LVISV05Dataset
 from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
-from .utils import replace_ImageToTensor
+from .utils import (NumClassCheckHook, get_loading_pipeline,
+                    replace_ImageToTensor)
 from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
@@ -20,5 +21,6 @@ __all__ = [
     'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
-    'build_dataset', 'replace_ImageToTensor', 'Bdd100kDataset', 'Bdd100k_ins_seg_Dataset'
+    'build_dataset', 'replace_ImageToTensor', 'Bdd100kDataset', 'Bdd100k_ins_seg_Dataset',
+    'get_loading_pipeline', 'NumClassCheckHook'
 ]
