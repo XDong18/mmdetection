ce1d5495384771c2bdc90be30e34894cf6e2902a
XDong18 <liuxd1215@gmail.com>
2021-02-05 20:19:38 +0800

    cc

--- configs/sparse_rcnn/sparse_rcnn_r101_fpn_300_proposals_crop_mstrain_480-800_3x_bdd100k.py
+++ configs/sparse_rcnn/sparse_rcnn_r101_fpn_300_proposals_crop_mstrain_480-800_3x_bdd100k.py
@@ -1,3 +1,7 @@
 _base_ = './sparse_rcnn_r50_fpn_300_proposals_crop_mstrain_480-800_3x_bdd100k.py'
 
 model = dict(pretrained='torchvision://resnet101', backbone=dict(depth=101))
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+)
